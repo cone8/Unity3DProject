9a0f8cd9435180a6967f98bb17e3cb31a12046af
cone8 <coneuk@gmail.com>
2016-03-15 23:09:09 +0200

    Генератор на стаи
    Направих прост генератор на стаи, като идея откъде да започнем. Все още
    е доста примитивно, но искам да дам някаква основа.

---
+++ Ampere/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    float h;
+    float v;
+    public float speed;
+    Rigidbody playerRB;
+
+    void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+	void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        playerRB.velocity = new Vector3(h * speed, 0f, v * speed);
+    }
+}
